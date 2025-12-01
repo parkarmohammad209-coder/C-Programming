bc1d756810fccaca7ab52fef341788eae2e882a8
parkarmohammad209-coder <parkarmohammad209@gmail.com>
2025-12-01 21:45:52 +0530

    Add files via upload

---
+++ exp_1_1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+
+    /*
+    NAME:- PARKAR MOHAMMED DILAWAR
+    CLASS:- COMPS F.Y. DIVISION:- C-2
+    UIN:- 251P043
+    */
+
+    printf("Name = PARKAR MOHAMMED DILAWAR\nAge = 18\nClass = F.Y. Comps\nDivision = C\nUIN = 251P043");//printed...!
+    return 0;
+}
